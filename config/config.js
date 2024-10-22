57fd7736f5d1aeead8936010538fa183602d6425
hqrealmaker <hq.realmaker@gmail.com>
2024-10-22 15:20:47 +0700

    rebuilt in trm

--- config/config.js
+++ config/config.js
@@ -2,11 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
+  mongoUri: 'mongodb+srv://doadmin:caQJA315R6XC4789@db-mongodb-sgp1-40734-d82ef568.mongo.ondigitalocean.com/admin?replicaSet=db-mongodb-sgp1-40734&tls=true&authSource=admin'
 }
 
 export default config
